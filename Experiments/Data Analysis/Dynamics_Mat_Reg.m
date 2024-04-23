902a208a90b9e6f30ad3241da42c682e19e35fb8
nikz123456 <135058244+nikz123456@users.noreply.github.com>
2024-04-22 21:20:57 -0400

    Updating Matlab Time Embedding
    The Dynamics Matrix Regression function has been updated to appropriately stack the kronecker products vertically rather than horizontally.
    This results in a regression to A and B vectorized matrices that are at least non-zero and more in line with the process that I believe Zac originally described.
    
    The Time Delay Embed function has been updated to output a true Hankel matrix in that the first columns are not couched with a constant initial condition if the time delay pre-dates the passed in matrix index. The quantity of its output columns are updated based on the quantity of time delays requested.
    
    A Time Embed Forward Rollout function has been added to calculate a simulated set of jaw pressures given a trajectory of inputs, initial condition, and dynamic system matrices. This can be used to check simulated results of the modeled A and B matrices versus actual experimental data. Uncertain if this is yet fully working as its results with time delay embedded systems are unstable which is counter-intuitive to how they should behave.
    
    The Hankel Exploration main function has been updated to use these helper functions accordingly and additionally has had some plotting code starting to be added to aid in visualizing results.

--- Experiments/Data Analysis/Dynamics_Mat_Reg.m
+++ Experiments/Data Analysis/Dynamics_Mat_Reg.m
@@ -16,36 +16,36 @@ kron_col_x = nx*nx;
 kron_col_u = nu*nx;
 
 % initialize the large kronecker matrix
-big_kron = zeros(nx, nm*(kron_col_x + kron_col_u));
+big_kron = zeros(nx*nm, (kron_col_x + kron_col_u));
 
-% initialize a place to sum up the x_k+1 values (ie x_k2)
-x_k2_summer = zeros(nx, 1); %% wait not sure about this size, it might need to be stacked not added
+% initialize a place to gather up the x_k+1 values (ie x_k2)
+x_k2_vector = zeros(nx*nm, 1); 
 
 % fill the large kronecker matrix
 for k=1:nm % need to do some careful indexing here
     x_start_pt = (k-1)*(kron_col_x+kron_col_u)+1;
     u_start_pt = x_start_pt+kron_col_x;
 
-    big_kron(:,x_start_pt:x_start_pt+kron_col_x-1) = kron(X_k(:,k)',I); 
-    big_kron(:,u_start_pt:u_start_pt+kron_col_u-1) = kron(U_k(:,k)',I);
+    big_kron(nx*(k-1)+1:nx*k, 1:kron_col_x) = kron(X_k(:,k)',I); 
+    big_kron(nx*(k-1)+1:nx*k, kron_col_x+1:kron_col_x+kron_col_u) = kron(U_k(:,k)',I);
 
     % in this iter, probably can build up the sum of x_k2s
-    x_k2_summer = x_k2_summer + X_k2(:,k);
+    x_k2_vector(nx*(k-1)+1:nx*k) = X_k2(:,k);
 end
 
 % pseudo inverse the summation of values over time with the kronecker
 % matrix to solve for least squares answer for a vectorized version of A
 % and B
-stacked_vectorized_matrices = big_kron\x_k2_summer;
+stacked_vectorized_matrices = big_kron\x_k2_vector;
 
 % trying some different inverses
 % apparently this comes out more equitably distributed than \ which
 % prioritizes zero elements
-moore_penrose_stacked_matrix = pinv(big_kron)*x_k2_summer;
+moore_penrose_stacked_matrix = pinv(big_kron)*x_k2_vector;
 
 % sb same answer as moores-penrose, but per matlab doc it's "typically"
 % more efficient
-ls_stacked_matrix = lsqminnorm(big_kron, x_k2_summer);
+ls_stacked_matrix = lsqminnorm(big_kron, x_k2_vector);
 
 % Use the first entries to extract/fill A
 for i=1:nx
