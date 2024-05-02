c560e1a76e996988fa50083bf01c0c8b391298c3
nikz123456 <135058244+nikz123456@users.noreply.github.com>
2024-05-02 17:33:56 -0400

    Tried time embedding on pendulum toy problem
    Eliminated any usage of the "Dynamics Matrix Regression" function that leverages the kron and vec tricks.
    
    Added a sweep of delays, fitting, plotting and RMSE for the unforced pendulum profile. Our time embedding approach appears to do very well at modeling the unforced pendulum position with 3 time delays. There is a very clear elbow in this RMSE plot for optimal time delay choice.

--- Experiments/Data Analysis/SimplePendulum.m
+++ Experiments/Data Analysis/SimplePendulum.m
@@ -40,7 +40,7 @@ xlabel('time (s)')
 ylabel('Angular velocity, $\dot{\theta}$ (rad/s))', 'Interpreter','latex')
 
 %% fit linear model to pendulum data using N4sid, and compare the A and B matrices
-figure()
+figure(2)
 nx = 1:5; %model order
 sys = n4sid(u.',x_RK4(1,:).', nx, 'Ts', dt);
 
@@ -62,26 +62,117 @@ disp(sys.C)
 
 
 %% Fit using homebrew dynamics regression
-gen_nx = 1; % using all three jaw states
-gen_nu = 1; % using a single commanded state
-gen_nm = length(x_RK4(1,:)); % number of measurements = length of first index
+nx = 1; % theta
+nu = 1; % torque
+nm = length(x_RK4(1,:)); % number of measurements = length of first index
 
-gen_X = zeros(gen_nx, gen_nm);
-gen_U = zeros(gen_nu, gen_nm);
+X = zeros(nx, nm);
+U = zeros(nu, nm);
 
-gen_X = x_RK4(1,:);
-gen_U(1,:) = u;
+X = x_RK4(1,:);
+U(1,:) = u;
 
-
-gen_U_k1 = gen_U(:, 1:end-1);
-gen_X_k1 = gen_X(:, 1:end-1);
-gen_X_k2 = gen_X(:, 2:end);
+% shifting to massage the data such that our collected data is in the form:
+% x_k2 = f(x_k1, u_k1)
+U_k1 = U(:, 1:end-1);
+X_k1 = X(:, 1:end-1);
+X_k2 = X(:, 2:end);
 
 % Naive A and B, i.e. without any time delays
-[naive_A, naive_B] = Dynamics_Mat_Reg(gen_X_k1, gen_U_k1, gen_X_k2, 1, 1);
+%[naive_A_tilde, naive_B_tilde] = Dynamics_Mat_Reg(gen_X_k1, gen_U_k1, gen_X_k2, 1, 1);
 
+% Fitting A and B w/o kron approach
+naive_A = zeros(nx, nx); % standard A is nx by nx
+naive_B = zeros(nx, nu); % standard B is nx by nu
 
+% stack controls and state for regression
+state_ctrl = [U_k1; X_k1];
 
+% linear regression to solve for dynamics matrices, will be in form: 
+% dynamics_matrix = [naive_B naive_A]
+dynamics_matrix = linsolve(state_ctrl', X_k2')';
+
+% sizes should match original declaration
+naive_B = dynamics_matrix(:, 1:nu);
+naive_A = dynamics_matrix(:, nu+1:end); 
+% (also these should match the original answer from vec trick, A_tilde,
+% B_tilde)
+
+%% Roll the naive stuff forward to compare (presumably this will perform terribly)
+init_cond = X_k1(:,1);
+naive_X_sim = Delay_no_kron_Forward_Rollout(dynamics_matrix, init_cond,...
+    nx, U_k1, 0);
+
+% shift the time over a step so it maps to our x_k+1 output
+sim_t = t(:,2:end);
+
+% (the plot does indeed look like nothing)
+%plot(sim_t, naive_X_sim(1,:),'--o', DisplayName = "Angular Position from Naive Linear Regression");
+
+%% Test some time delay embedding
+
+% some sample delay quantities
+test_delays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50];
+
+% cell matrices to store x_k, x_k+1, u_k, [B A_i] for all tested delays
+% first row = x_k, second row = x_k+1
+% third row = rollout simulation, fourth row = time (for plotting)
+X_delays = cell(5, length(test_delays));
+U_delays = cell(1, length(test_delays));
+dyn_mat_delays = cell(1, length(test_delays));
+
+errors = zeros(1, length(test_delays));
+
+% generate respective time delay embedded matrices
+for i=1:length(test_delays)
+    % x_k gets the time delays embedded into it
+    X_delays{1, i} = Time_Delay_Embed(X_k1, test_delays(i));
+    
+    % x_k2, u_k, & time just get trimmed so their length/entries stay in
+    % alignment
+    % (ie if x_k starts at 2, then we need u_k to also start at 2)
+    U_delays{1, i} = U_k1(:, test_delays(i)+1:end);
+    X_delays{2, i} = X_k2(:, test_delays(i)+1:end);
+    X_delays{4, i} = sim_t(:, test_delays(i)+1:end); % time row
+    
+    % stack controls and state for regression
+    state_ctrl = [U_delays{1, i}; X_delays{1, i}];
+
+    % regression step
+    dyn_mat_delays{1, i} = linsolve(state_ctrl', X_delays{2,i}')';
+
+    % rolling the system out
+    init_cond = X_delays{1, i}(:,1);
+    X_delays{3, i} = Delay_no_kron_Forward_Rollout(dyn_mat_delays{1, i},...
+        init_cond, nx, U_delays{1,i}, test_delays(i));
+
+    % calc RMSE between rollout and actual
+    errors(i) = rmse(X_delays{3,i}, X_delays{2,i});
+end
+
+% ex) test_delay(1) => 1 delay
+%plot(X_delays{4, 1}, X_delays{3,1},'--+', DisplayName = "1 Delay");
+
+figure(3)
+legend
+hold on
+xlabel('Time (s)')
+ylabel('Angular position, $\theta$ (rad)', 'Interpreter','latex')
+plot(t,x_RK4(1,:),'r-', DisplayName = "Position")
+plot(X_delays{4, 1}, X_delays{3,1},'b-', DisplayName = "1 Delay");
+plot(X_delays{4, 3}, X_delays{3,3},'g--', DisplayName = "3 Delay");
+
+figure(4)
+hold on
+xlabel('Number of Time Delays')
+ylabel('RMSE with Actual Data')
+plot(test_delays, errors, '-o')
+
+figure(2)
+plot(X_delays{4, 3}, X_delays{3,3},'--o', DisplayName = "3 Delay");
+plot(X_delays{4, 2}, X_delays{3,2},'--o', DisplayName = "2 Delay")
+
+%% Dynamics and integration functions
 function x_dot = pendulum_dynamics_continuous(x,u,m,g,l)
     x_dot=zeros(2,1);
     x_dot(1) = x(2); %velocity kinematics
